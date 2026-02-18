06ea9954a8609be7524e9e92f03919f89107ffd7
shoriful-dev <cmshohelrana50@gmail.com>
2026-02-18 23:37:54 +0600

    add SocialProof section

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from '@/components/landing/hero';
 import Navber from '@/components/landing/nav';
+import SocialProof from '@/components/landing/social';
 
 const Page = () => {
   return (
     <main className="w-full flex flex-col relative z-10">
       <Navber />
       <Hero />
+      <SocialProof />
     </main>
   );
 };
